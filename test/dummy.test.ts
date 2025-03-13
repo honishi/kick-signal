1315d545c6bf804514564f71d50e689b7f4ec60d
Hiroyuki Onishi <onishi.hiroyuki@gmail.com>
2025-03-13 22:03:21 +0900

    remove unused test

--- test/dummy.test.ts
+++
@@ -1,6 +0,0 @@
-describe('Hello World', () => {
-  it('checks if the string equals "Hello World"', () => {
-    const greeting = 'Hello World';
-    expect(greeting).toBe('Hello World');
-  });
-});
